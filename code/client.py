0460c2311ea23d07e1b1dba46fb3232cc9b8a371
Sujaya A Maiyya <sujaya@Sujaya.local>
2017-01-22 16:13:21 -0800

    Mostly working code!

--- code/client.py
+++ code/client.py
@@ -2,61 +2,35 @@ import socket
 import time
 import asyncore
 import logging
+import json, sys
 logging.basicConfig(filename='client.log',level=logging.DEBUG)
-# class Client:
-#     def buyTickets(self, dcId, dcInfo, noOfTickets):
 
-#         ip, port = dcInfo[0], dcInfo[1]
-#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         s.connect((ip, port))
-#         time.sleep(30)
-#         reqMsg = 'CLIREQ:'+dcId+','+str(noOfTickets)
-#         s.send(reqMsg)
-#         data = s.recv(4096)
-#         s.close()
-#         print('Received', repr(data))
+with open('server_config.json') as config_file:    
+    config = json.load(config_file)
 
-class Client(asyncore.dispatcher):
-    """Sends messages to the server and receives responses.
-    """
-    
-    def buyTickets(self, dcId, dcInfo, noOfTickets):
-    	ip, port = dcInfo[0], dcInfo[1]
-        self.sendMsg = 'CLIREQ:'+dcId+','+str(noOfTickets)
-        self.recvMsg = ''
-        self.chunk_size = 4096
-        self.logger = logging.getLogger('EchoClient')
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.logger.debug('connecting to %s', (ip, port))
-        self.connect((ip, port))
-        return
-        
-    def handle_connect(self):
-        self.logger.debug('handle_connect()')
-    
-    def handle_close(self):
-        self.logger.debug('handle_close()')
-        self.close()
-        #self.logger.debug('Received Data: '+ self.recvMsg)
-        return
-    
-    def writable(self):
-        self.logger.debug('writable() -> %s', bool(self.sendMsg))
-        return bool(self.sendMsg)
 
-    def handle_write(self):
-        self.send(self.sendMsg)
-        self.logger.debug('handle_write() -> "%s"', self.sendMsg)
-        self.sendMsg = ''
+delay = int(sys.argv[1])
+BUFFER_SIZE = 2000 
+ 
+ 
+while True:
+    dcId = raw_input("Enter DataCenter ID/ Enter 0 to exit.:")
+    if dcId == '0':
+        break
+    noOfTickets = raw_input("Enter no. of tickets: ")
 
-    def handle_read(self):
-        self.recvMsg = self.recv(self.chunk_size)
-        self.logger.debug('handle_read() -> "%s"', self.recvMsg)
-        self.handle_close()
-        
+    ip, port = config["datacenters"][dcId][0], config["datacenters"][dcId][1]
+    reqMsg = 'CLIREQ::'+dcId+','+str(noOfTickets)+','+json.dumps({'seqNo':0})
 
-c1 = Client()
-#while True:
-c1.buyTickets('dc1', ['127.0.0.1', 5001], 3)
-asyncore.loop()
+    tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    time.sleep(delay)
+    tcpClient.connect((ip, port))
+    tcpClient.send(reqMsg)
+
+    data = tcpClient.recv(BUFFER_SIZE)
+    while not data:
+        data = tcpClient.recv(BUFFER_SIZE)
+    print data
+
+    tcpClient.close() 
+
--- code/server.py
+++ code/server.py
@@ -1,153 +1,222 @@
-import socket
-import json
+import socket 
+from threading import Thread 
+from SocketServer import ThreadingMixIn 
 import time
-import asyncore
+import threading
+import json, sys
 import logging
+import sys
 
-logging.basicConfig(filename='server.log',level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+delay = 2
 REQ = 'REQ'
 REL = 'REL'
 REP = 'REP'
 CLIREQ = 'CLIREQ'
+with open('server_config.json') as config_file:    
+    config = json.load(config_file)
 
 
-# class Server:
+class MutexInfo:
+	def __init__(self, seqNo, procId, selfDcId, conn=None):
+		self.clock = {'seqNo':seqNo, 'procId':procId}
+		self.totalTickets = config['tickets']
+		self.totalDCs = len(config['datacenters'])
+		#prQueue = [(dc1, Clock), (dc2, Clock)]
+		self.prQueue = []
+		self.waitingForRelease = False
+		self.selfDcId = selfDcId
+		self.requestedNoOfTickets = 0
+		self.clientConn = conn
 
-# 	def initialize(self, dcId, config):
+
+
+	def comparator(self, tup1, tup2):
+		clockA, clockB = tup1[1], tup2[1]
+		if clockA['seqNo'] < clockB['seqNo']:
+			return -1
+		elif clockA['seqNo'] == clockB['seqNo'] and clockA['procId'] < clockB['procId']:
+			return -1
+		return 1
+
+
+# Multithreaded Python server : TCP Server Socket Thread Pool
+class ClientThread(Thread): 
+ 
+	def __init__(self, conn, ip, port, mutexinfo): 
+		Thread.__init__(self) 
+		self.ip = ip 
+		self.port = port 
+		self.conn = conn
+		self.mutexinfo = mutexinfo
+		#print "[+] New server socket thread started for " + ip + ":" + str(port) 
+ 
+	def run(self): 
+		cliReq = False
+		conn, recvMsg = self.conn, self.conn.recv(2048)
+		print "Server received data:", recvMsg
+
+		msgType, dcId, noOfTickets, clock = self.parseRecvMsg(recvMsg)
+		self.updateClock(self.mutexinfo.clock['seqNo'], clock['seqNo'])
+
+		if msgType==CLIREQ:
+			cliReq = True
+			self.handleClientReq(noOfTickets)
+		elif msgType == REQ:
+			self.handleDatacenterReq(dcId, noOfTickets, clock)
+		elif msgType == REP:
+			self.handleReplyMsg()
+		elif msgType == REL:
+			self.handleReleaseMsg(dcId, noOfTickets)
+
+		if not cliReq:
+			conn.close() 
+		sys.exit()
+
+
+	def parseRecvMsg(self, recvMsg):
+		msgType, dcId, noOfTickets, clock = recvMsg.split('::')[0], \
+						recvMsg.split('::')[1].split(',')[0], \
+						recvMsg.split('::')[1].split(',')[1], \
+						recvMsg.split('::')[1].split(',', 2)[2]
 		
-# 		ip, port = config["datacenters"][dcId][0], config["datacenters"][dcId][1]
-
-# 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 		#s.setblocking(0)
-# 		s.bind((ip, port))
-# 		s.listen(1)
-# 		while True:
-# 			conn, addr = s.accept()
-# 			print('Connected by ', addr)
-# 			recvMsg = conn.recv(4096)
-# 			print(recvMsg)
-# 			'''Keep reaceving data as long as data is being sent.'''
-# 			#while True:
-# 		  	#  data = conn.recv(1024)
-# 		    #  if not data: break
-# 		    #  recvMsg += data
-
-# 			msgType, dcId, noOfTickets = self.parseRecvMsg(recvMsg)
-# 			time.sleep(30)
-# 			'''Perform appropriate action based on type of message received'''
-# 			if msgType == REQ:
-# 				print REQ
-# 			elif msgType == REP:
-# 				print REP
-# 			elif msgType == REL:
-# 				print REL
-# 			elif msgType == CLIREQ:
-# 				print CLIREQ
-
-# 			time.sleep(5)
-# 			conn.send(recvMsg)
-# 			conn.close()
-
-class Tickets:
-	def __init__(self, tickets):
-		self.tickets = tickets
-class Server(asyncore.dispatcher):
-    """Receives connections and establishes handlers for each client.
-    """
-
-    
-    def __init__(self, dcId, config):
-
-        self.logger = logging.getLogger('EchoServer')
-        ip, port = config["datacenters"][dcId][0], config["datacenters"][dcId][1]
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.bind((ip, port))
-        self.address = self.socket.getsockname()
-        self.logger.debug('binding to %s', self.address)
-        self.listen(5)
-        self.tickets = Tickets(10)
-        return
-
-    def handle_accept(self):
-        # Called when a client connects to our socket
-        client_info = self.accept()
-        self.logger.debug(str(time.time()) + 'handle_accept() -> %s', client_info[1])
-        print 'Accepted'
-        Handler(sock=client_info[0], tickets=self.tickets)
-        #self.handle_close()
-        return
-    
-    def handle_close(self):
-        self.logger.debug('handle_close()')
-        self.close()
-        return
-
-class Handler(asyncore.dispatcher):
-    """Handles echoing messages from a single client.
-    """
-    
-    def __init__(self, sock, tickets, chunk_size=4096):
-    	tickets.tickets = tickets.tickets-1
-        self.chunk_size = chunk_size
-        self.data_to_write = ''
-        self.logger = logging.getLogger('Handler%s' % str(sock.getsockname()))
-        asyncore.dispatcher.__init__(self, sock=sock)
-        print tickets.tickets
-        return
-
-    def parseRecvMsg(self, recvMsg):
-		msgType, dcId, noOfTickets = recvMsg.split(':')[0], \
-								recvMsg.split(':')[1].split(',')[0], \
-								recvMsg.split(':')[1].split(',')[1]
-
-		return msgType, dcId, noOfTickets
-
-    def writable(self):
-        """We want to write if we have received data."""
-        response = bool(self.data_to_write)
-        self.logger.debug('writable() -> %s', response)
-        return response
-    
-    def handle_write(self):
-        """Write as much as possible of the most recent message we have received."""
-        data = self.data_to_write
-        sent = self.send(data)
-        self.logger.debug('handle_write() -> "%s"', data)
-        #if not self.writable():
-        self.handle_close()
-
-    def handle_read(self):
-        """Read an incoming message from the client and put it into our outgoing queue."""
-        recvMsg = self.recv(self.chunk_size)
-        #We may have to add sleep
-        if not recvMsg:
-        	self.handle_close()
-        self.logger.debug('handle_read() -> "%s"', recvMsg)
-
-        msgType, dcId, noOfTickets = self.parseRecvMsg(recvMsg)
-        time.sleep(30)
-        '''Perform appropriate action based on type of message received'''
-        if msgType == REQ:
-        	print REQ
-        elif msgType == REP:
-        	print REP
-        elif msgType == REL:
-        	print REL
-        elif msgType == CLIREQ:
-        	#self.handleClientRequest()
-        	print CLIREQ
-
-		self.data_to_write = "From server: " + recvMsg
-
-	def handle_close(self):
-		self.logger.debug('handle_close()')
-		self.close()
+		clock = json.loads(clock)
+		return msgType, dcId, int(noOfTickets), clock
 
 
-	#def handleClientRequest(self):
+	def sendTcpMsg(self, ip, port, msg):
+		tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+		time.sleep(delay)
+		tcpClient.connect((ip, port))
+		tcpClient.send(msg)
+		logMsg = 'Sent message to: (%s, %d). Message is: %s' %(ip, port, msg)
+		logger.debug(logMsg)
 
-with open('server_config.json') as data_file:    
-    data = json.load(data_file)
-s = Server('dc1', data)
-asyncore.loop()
+
+	def updateClock(self, seqNo1, seqNo2):
+		#increment clock value by finding max of current dc and sent dc + 1
+		self.mutexinfo.clock['seqNo'] = max(seqNo1, seqNo2) + 1
+		logMsg = 'Updated clock values to %d:%d' %(self.mutexinfo.clock['seqNo'], self.mutexinfo.clock['procId'])
+		logger.debug(logMsg)
+
+	def addAndUpdateQueue(self, dcId, clock):
+		self.mutexinfo.prQueue.append((dcId, clock))
+		self.mutexinfo.prQueue.sort(cmp=self.mutexinfo.comparator)
+		logMsg = 'Updated queue to %s' %repr(self.mutexinfo.prQueue)
+		logger.debug(logMsg)
+
+
+	def handleClientReq(self, noOfTickets):
+		self.mutexinfo.clientConn = self.conn
+		if self.mutexinfo.totalTickets - noOfTickets < 0:
+			self.replyToClient(success=False)
+			return
+
+		self.mutexinfo.requestedNoOfTickets = noOfTickets
+		clock = dict(self.mutexinfo.clock)
+		self.addAndUpdateQueue(self.mutexinfo.selfDcId, clock)
+		for dcId in config["datacenters"]:
+			if dcId == self.mutexinfo.selfDcId:
+				continue
+			ip, port = config["datacenters"][dcId][0], config["datacenters"][dcId][1]
+			reqMsg = 'REQ::'+self.mutexinfo.selfDcId+','+str(noOfTickets)+','+json.dumps(clock)
+			self.sendTcpMsg(ip, port, reqMsg)
+
+
+	def handleDatacenterReq(self, dcId, noOfTickets, clock):
+		# Queue appropriately and reply
+		self.addAndUpdateQueue(dcId, clock)
+		ip, port = config["datacenters"][dcId][0], config["datacenters"][dcId][1]
+		repMsg = 'REP::'+self.mutexinfo.selfDcId+','+str(noOfTickets)+','+json.dumps(self.mutexinfo.clock)
+		self.sendTcpMsg(ip, port, repMsg)
+
+
+	def handleReplyMsg(self):
+		self.mutexinfo.totalDCs -= 1
+		
+		if self.mutexinfo.totalDCs == 1:
+			self.accessTickets()
+	
+
+	def accessTickets(self):
+		
+		if self.mutexinfo.prQueue[0][0] == self.mutexinfo.selfDcId:
+			if self.mutexinfo.totalTickets - self.mutexinfo.requestedNoOfTickets < 0:
+				self.replyToClient(success=False)
+				return
+			# Check for top of Queue; if I am at top, reduct tickets value
+			self.mutexinfo.totalTickets -= self.mutexinfo.requestedNoOfTickets
+			logMsg = 'Updated tickets value to %d' %self.mutexinfo.totalTickets
+			logger.debug(logMsg)
+
+			#reset all variables
+			self.mutexinfo.requestedNoOfTickets = 0
+			self.mutexinfo.waitingForRelease = False
+			self.mutexinfo.totalDCs = len(config['datacenters'])
+			#release to other DCs
+			self.mutexinfo.prQueue.pop(0)
+			self.sendReleaseMsg()
+
+			#reply to client
+			self.replyToClient(success=True)
+		else:
+			logMsg = 'Waiting for other DCs to release.'
+			logger.debug(logMsg)
+			self.mutexinfo.waitingForRelease = True
+
+
+	def replyToClient(self, success):
+		if not success:
+			cliReply = 'Total tickets available: '+str(self.mutexinfo.totalTickets)+'. Tickets requested should be less that total tickets available.'
+			 			
+		else:
+			cliReply = 'Successfully purchased tickets. Total remaining tickes are ' +str(self.mutexinfo.totalTickets)
+			 			
+		self.mutexinfo.clientConn.send(cliReply)
+		self.mutexinfo.clientConn.close()
+		self.mutexinfo.clientConn = None
+
+
+	def sendReleaseMsg(self):
+		clock = dict(self.mutexinfo.clock)
+		tickets = self.mutexinfo.totalTickets
+		for dcId in config["datacenters"]:
+			if dcId == self.mutexinfo.selfDcId:
+				continue
+			ip, port = config["datacenters"][dcId][0], config["datacenters"][dcId][1]
+			relMsg = 'REL::'+self.mutexinfo.selfDcId+','+str(tickets)+','+json.dumps(clock)
+			self.sendTcpMsg(ip, port, relMsg)
+
+
+	def handleReleaseMsg(self, dcId, noOfTickets):
+		self.mutexinfo.totalTickets = noOfTickets
+		#pop dcid from queue
+		self.mutexinfo.prQueue.pop(0)
+		if self.mutexinfo.waitingForRelease:
+			self.accessTickets()
+
+######################################## Main ################################################
+
+
+selfDcId = sys.argv[1]
+ip, port = config["datacenters"][selfDcId][0], config["datacenters"][selfDcId][1]
+
+tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+tcpServer.bind((ip, port))
+
+mutexinfo = MutexInfo(0, int(selfDcId[2:]), selfDcId)
+threads = [] 
+print 'Server ready to listen on (%s:%d)' %(ip, port)
+while True: 
+    tcpServer.listen(4) 
+    (conn, (cliIP,cliPort)) = tcpServer.accept() 
+    newthread = ClientThread(conn, cliIP, cliPort, mutexinfo) 
+    newthread.start()
+    threads.append(newthread) 
+ 
